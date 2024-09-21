096647d9e3ce5901f47b5798c6b18e2c05533e0d
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-09-21 14:19:04 -0300

    first commit

---
+++ src/main/java/crudweb2/crudweb2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package crudweb2.crudweb2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Crudweb2Application.class);
+	}
+
+}
---
+++ src/test/java/crudweb2/crudweb2/Crudweb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package crudweb2.crudweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crudweb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
