0288945c18b0e1f6bfbdb34e024251b1ba185021
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-09-22 16:10:43 -0300

    Updated file script.sql to fix the code to create table matricula

--- script.sql
+++ script.sql
@@ -20,16 +20,6 @@ create table matricula(
     aluno_id integer not null,
     data_matricula varchar(8) not null,
     nota real not null,
-    constraint matricula_fk_curso references curso_id foreign key curso(id),
-    constraint matricula_fk_aluno references aluno_id foreign key aluno(id)
-);
-
-create table matricula(
-    id integer primary key generated always as identity,
-    curso_id integer not null,
-    aluno_id integer not null,
-    data_matricula varchar(8) not null,
-    nota real not null,
     constraint matricula_fk_curso foreign key(curso_id) references curso(id),
     constraint matricula_fk_aluno foreign key(aluno_id) references aluno(id)
 );
