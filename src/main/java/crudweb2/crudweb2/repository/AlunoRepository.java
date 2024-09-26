11ff67177e0dae56ea0e82089eb22d4248223f50
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-09-25 22:45:00 -0300

    Implement AlunoRepository and AlunoREST

--- src/main/java/crudweb2/crudweb2/repository/AlunoRepository.java
+++ src/main/java/crudweb2/crudweb2/repository/AlunoRepository.java
@@ -1,9 +1,10 @@
 package crudweb2.crudweb2.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import crudweb2.crudweb2.model.Aluno;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Integer>{
-    
+    public Optional<Aluno> findAlunoByCpf(String cpf);
 }
--- src/main/java/crudweb2/crudweb2/rest/AlunoREST.java
+++ src/main/java/crudweb2/crudweb2/rest/AlunoREST.java
@@ -7,12 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import crudweb2.crudweb2.model.Aluno;
 import crudweb2.crudweb2.repository.AlunoRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 @CrossOrigin
 @RestController
@@ -34,6 +40,41 @@ public class AlunoREST {
         } else{
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-    }    
+    } 
     
+    @PostMapping("/alunos")
+    public ResponseEntity<Aluno> insertAluno(@RequestBody Aluno aluno) {
+        Optional<Aluno> op = alunoRepository.findAlunoByCpf(aluno.getCpf());
+        if (op.isPresent()){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+        else{
+            aluno.setId(-1);
+            alunoRepository.save(aluno);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+    }
+
+    @PutMapping("/alunos/{id}")
+    public ResponseEntity<Aluno> updateAluno(@PathVariable int id, @RequestBody Aluno aluno) {
+        Optional<Aluno> op = alunoRepository.findById(id);
+        if (op.isPresent()){
+            aluno.setId(id);
+            alunoRepository.save(aluno);
+            return ResponseEntity.ok(aluno);
+        } else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(op.get());
+        }
+    }
+    
+    @DeleteMapping
+    public ResponseEntity<Aluno> deleteAluno(@PathVariable int id){
+        Optional<Aluno> op = alunoRepository.findById(id);
+        if (op.isPresent()){
+            alunoRepository.delete(op.get());
+            return ResponseEntity.ok(op.get());
+        } else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 }
