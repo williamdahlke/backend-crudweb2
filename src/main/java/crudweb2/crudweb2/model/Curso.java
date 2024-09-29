a635c15ffd03abdbed2e7dd89e0229c9dd2768dd
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-09-29 16:48:40 -0300

    Fixed import in Curso.java

--- src/main/java/crudweb2/crudweb2/model/Curso.java
+++ src/main/java/crudweb2/crudweb2/model/Curso.java
@@ -3,7 +3,6 @@ package crudweb2.crudweb2.model;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
