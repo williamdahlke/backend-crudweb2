3a14f6543e202b141258f4b2764d4a612fa81af7
William Guilherme Dahlke <williamgdahlke@gmail.com>
2024-09-29 11:22:34 -0300

    Fixed script.sql

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -5,7 +5,7 @@ create table aluno(
     nome varchar(256) not null,
     cpf varchar(11) not null,
     email varchar(512) not null,
-    dt_nascimento varchar(8) not null
+    dt_nascimento varchar(10) not null
 );
 
 create table curso(
@@ -18,7 +18,7 @@ create table matricula(
     id integer primary key generated always as identity,
     curso_id integer not null,
     aluno_id integer not null,
-    data_matricula varchar(8) not null,
+    data_matricula varchar(10) not null,
     nota real not null,
     constraint matricula_fk_curso foreign key(curso_id) references curso(id),
     constraint matricula_fk_aluno foreign key(aluno_id) references aluno(id)
